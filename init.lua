befefd3bb4510cb952f991620bae28911f72a08c
Vladimir Antin <vantin@cscsw.com>
2026-04-07 11:35:34 +0200

    prettier + eslint

--- init.lua
+++ init.lua
@@ -1,74 +1,100 @@
--- Osnovna podešavanja
+-- 1. OSNOVNA PODEŠAVANJA (Options)
 vim.g.mapleader = " "
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.mouse = 'a'         -- Omogućava miš (korisno na početku)
+vim.opt.mouse = 'a'
 vim.opt.termguicolors = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.cursorline = true
 
--- Automatska instalacija Lazy.nvim (Plugin Manager)
+-- 2. LAZY.NVIM INSTALACIJA
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath,
-  })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Lista pluginova
+-- 3. PLUGINI
 require("lazy").setup({
-  -- Tema (Izgled)
+  -- Izgled
   { "folke/tokyonight.nvim", lazy = false, priority = 1000, config = function() vim.cmd[[colorscheme tokyonight]] end },
-  
-  -- Statusna linija (Dole)
   { 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' }, config = true },
-  
-  -- Syntax Highlighting (Boje koda)
-  { 
-    "nvim-treesitter/nvim-treesitter", 
+
+  -- TREESITTER (Ispravljen)
+  {
+    "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    lazy = false, -- Ovo osigurava da se učita odmah
     config = function()
-      -- Koristimo pcall (protected call) da ne bi pukao ceo Neovim ako modul fali
+      -- Koristimo pcall da sprečimo "module not found" grešku pri prvom paljenju
       local ok, configs = pcall(require, "nvim-treesitter.configs")
-      if not ok then 
-        return 
-      end
-      
+      if not ok then return end
       configs.setup({
-        ensure_installed = { "lua", "vim", "vimdoc", "javascript", "python" },
+        ensure_installed = { "lua", "vim", "javascript", "typescript", "json", "html", "css" },
         highlight = { enable = true },
       })
-    end 
+    end
   },
 
-  -- Sidebar (Fajl menadžer) - otvara se sa :NvimTreeToggle
-  { "nvim-tree/nvim-tree.lua", config = true },
+  -- Sidebar
+  { "nvim-tree/nvim-tree.lua", config = { actions = { open_file = { quit_on_open = true } } } },
 
-  -- Telescope (Brza pretraga fajlova)
+  -- Telescope
   { 'nvim-telescope/telescope.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
+
+  -- LSP & FORMATTING (Moderni 0.12 pristup)
+  {
+    "williamboman/mason.nvim",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "neovim/nvim-lspconfig",
+      "nvimtools/none-ls.nvim",
+    },
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "ts_ls", "eslint", "html", "cssls" }
+      })
+
+      -- NOVI SISTEM: Koristimo vim.lsp.enable (Neovim 0.11/0.12+)
+      local servers = { "ts_ls", "eslint", "html", "cssls" }
+      for _, server in ipairs(servers) do
+        vim.lsp.enable(server)
+      end
+
+      -- Prettier (None-ls)
+      local ok, null_ls = pcall(require, "null-ls")
+      if ok then
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+        null_ls.setup({
+          sources = {
+            null_ls.builtins.formatting.prettier.with({
+              condition = function(utils)
+                return utils.root_has_file({ ".prettierrc", ".prettierrc.json", ".prettierrc.js", "package.json" })
+              end,
+            }),
+          },
+          on_attach = function(client, bufnr)
+            if client.supports_method("textDocument/formatting") then
+              vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+              vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                  vim.lsp.buf.format({ bufnr = bufnr, filter = function(c) return c.name == "null-ls" end })
+                end,
+              })
+            end
+          end,
+        })
+      end
+    end
+  },
 })
 
-
--- Brže kretanje između prozora (nema potrebe za ctrl+w)
-vim.keymap.set('n', '<C-h>', '<C-w>h') -- Ctrl+h za levo
-vim.keymap.set('n', '<C-j>', '<C-w>j') -- Ctrl+j za dole
-vim.keymap.set('n', '<C-k>', '<C-w>k') -- Ctrl+k za gore
-vim.keymap.set('n', '<C-l>', '<C-w>l') -- Ctrl+l za desno
-
--- Brzo kretanje kroz buffere (kao tabovi u Chrome-u)
--- Shift + l za sledeći, Shift + h za prethodni
-vim.keymap.set('n', '<S-l>', ':bnext<CR>')
-vim.keymap.set('n', '<S-h>', ':prev<CR>')
-
--- Prečica za File Explorer (nvim-tree)
-vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>') -- Space + e otvara sidebar
-
--- Prečica za Telescope (traženje fajlova)
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {}) -- Space + ff traži fajlove
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})  -- Space + fg traži tekst u fajlovima
-
--- vim.opt.autochdir = true
+-- 4. PREČICE
+local keymap = vim.keymap.set
+keymap('n', '<leader>e', ':NvimTreeToggle<CR>')
+keymap('n', '<leader>ff', ':Telescope find_files<CR>')
+keymap('n', 'gd', vim.lsp.buf.definition)
+keymap('n', 'K', vim.lsp.buf.hover)
